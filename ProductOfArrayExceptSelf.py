d954ac4b226a3f66354f7df2f2b1fc6855208f61
asmifirdaus <asmifirdaus2@gmail.com>
2024-11-28 18:36:29 +0530

    Create ProductOfArrayExceptSelf.py

---
+++ ProductOfArrayExceptSelf.py
@@ -0,0 +1,18 @@
+#https://leetcode.com/problems/product-of-array-except-self/
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        left=[1]*len(nums)
+        right=[1]*len(nums)
+        result=[1]*len(nums)
+        for i in range(1,len(nums)):
+            left[i]=left[i-1]*nums[i-1]
+        for i in range(len(nums)-2,-1,-1):
+            right[i]=right[i+1]*nums[i+1]
+        print(left,right)
+        for i in range(len(nums)):
+            result[i]=left[i]*right[i]
+        return result
+
+            
+        
